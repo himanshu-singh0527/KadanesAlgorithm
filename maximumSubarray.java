b27797e1406d87415f0c5a01835926dd633d44e4
himanshu-singh0527 <144667707+himanshu-singh0527@users.noreply.github.com>
2024-07-30 22:47:35 +0530

    Add files via upload

---
+++ maximumSubarray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+}
+public class maximumSubarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nums[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        Solution solution = new Solution();
+        int maxSubArraySum = solution.maxSubArray(nums);
+        System.out.println("Max subarray sum is: " + maxSubArraySum);
+    }
+}
